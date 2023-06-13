7f69f11d257c687a58c75858044a3e5ffad9dbda
BrenoUni14 <breno.carmo01@fatec.sp.gov.br>
2023-06-13 17:08:18 -0300

    página de consulta e delete arrumados e arrumando página de edição

--- app.js
+++ app.js
@@ -6,129 +6,133 @@ const handlebars = require("express-handlebars").engine
 
 app.engine("handlebars", handlebars({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({ extend: false }))
-app.use(bodyParser.json())
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
 
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app')
-const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore')
-
-const serviceAccount = require("./fatecweb-32840-firebase-adminsdk-3q5xm-676ed51519.json")
+const serviceAccount = require("./fatecweb-32840-firebase-adminsdk-3q5xm-676ed51519.json");
 
 initializeApp({
     credential: cert(serviceAccount)
-})
+});
 
-const db = getFirestore()
-
+const db = getFirestore();
 
 //---------------------------------------------------------------/
 //criando uma rota para o diretório principal do domínio
 app.get("/", function (req, res) {
-    res.render("primeira_pagina")
-})
+    res.render("primeira_pagina");
+});
 
 //---------------------------------------------------------------/
-//criando uma rota para a cadastrar
+//criando uma rota para cadastrar
 app.post("/cadastrar", function (req, res) {
-    var adicionar = db.collection('agendamentos').add({
+    db.collection('agendamentos')
+        .add({
+            nome: req.body.nome,
+            telefone: req.body.telefone,
+            origem: req.body.origem,
+            data: req.body.data,
+            observacao: req.body.observacao
+        })
+        .then(() => {
+            console.log("Document added");
+            res.render("primeira_pagina");
+        })
+        .catch((error) => {
+            console.log("Error adding document: ", error);
+            res.render("primeira_pagina");
+        });
+});
+
+//---------------------------------------------------------------/
+//criando uma rota para a página de consulta
+app.get('/consulta', (req, res) => {
+    db.collection('agendamentos')
+        .get()
+        .then((snapshot) => {
+            const documents = [];
+            snapshot.forEach((doc) => {
+                const documentData = doc.data();
+                documentData.id = doc.id; // Adiciona a propriedade 'id' ao documento
+                documents.push(documentData);
+            });
+            res.render('consulta', { documents });
+        })
+        .catch((error) => {
+            console.log('Erro ao consultar o Firestore: ', error);
+            res.render('consulta', { documents: [] });
+        });
+});
+
+//---------------------------------------------------------------/
+// Criando uma rota para a página de edição
+app.get("/editar/:id", function (req, res) {
+    const documentId = req.params.id;
+    db.collection('agendamentos')
+        .doc(documentId)
+        .get()
+        .then((doc) => {
+            if (doc.exists) {
+                const documentData = doc.data();
+                documentData.id = doc.id; // Adiciona a propriedade 'id' ao documento
+                res.render('editar', { post: documentData }); // Renderiza o template 'editar' com os dados do documento
+            } else {
+                console.log('Documento não encontrado');
+                res.redirect('/consulta');
+            }
+        })
+        .catch((error) => {
+            console.log('Erro ao consultar o Firestore: ', error);
+            res.redirect('/consulta');
+        });
+});
+
+//---------------------------------------------------------------/
+//criando uma rota para a página de exclusão
+app.get("/deletar/:id", function (req, res) {
+    const documentId = req.params.id;
+    db.collection('agendamentos')
+        .doc(documentId)
+        .delete()
+        .then(() => {
+            console.log('Documento excluído com sucesso');
+            res.redirect('/consulta');
+        })
+        .catch((error) => {
+            console.log('Erro ao excluir o documento: ', error);
+            res.redirect('/consulta');
+        });
+});
+
+//---------------------------------------------------------------/
+//criando uma rota para a atualização na consulta
+app.post("/atualizar", function (req, res) {
+    const documentId = req.body.id;
+    const updatedDocument = {
         nome: req.body.nome,
         telefone: req.body.telefone,
         origem: req.body.origem,
         data: req.body.data,
         observacao: req.body.observacao
-    })
-
-    console.log("Added document");
-    res.render("primeira_pagina")
-})
-
-//---------------------------------------------------------------/
-//criando uma rota para a página de consulta
-app.get('/consulta', (req, res) => {
-    // Consulta ao Firestore
+    };
     db.collection('agendamentos')
-  .get()
-  .then((snapshot) => {
-    const documents = [];
-    snapshot.forEach((doc) => {
-      const documentData = doc.data();
-      documentData.id = doc.id; // Adiciona a propriedade 'id' ao documento
-      documents.push(documentData)
-    });
-    res.render('consulta', { documents });
-  })
-  .catch((error) => {
-    console.log('Erro ao consultar o Firestore: ', error);
-    res.render('consulta',  { documents: [] });
-  })
-})
-
-
-
-/*
-    const collectionRef = db.collection('agendamentos');
-    collectionRef.get()
-        .then(snapshot => {
-            snapshot.forEach(doc => {
-
-                // Cria uma referência para um documento em outra coleção usando o ID do documento atual
-                const cityRef = db.collection('agendamentos').doc(doc.id);
-
-                console.log(doc)
-
-
-
-                res.render("consulta", { unity: arr })
-                // Obtém o documento usando a referência
-                // const unity = cityRef.get();
-
-                // Acessa os dados do documento obtido
-                cityRef.get().then(unity => {
-                    const arr = []
-                    if (unity && unity.exists) {
-                        unity.forEach(unity => {
-                            arr.push(unity.data())
-                        })
-                        //Imprime o ID do documento e seus dados
-                        console.log(unity.id, '=>', unity.data());
-                    }
-
-                })
-                    .catch(err => {
-                        // Trata erros ao obter o documento
-                        console.log(err);
-                    });
-            });
+        .doc(documentId)
+        .update(updatedDocument)
+        .then(() => {
+            console.log('Documento atualizado com sucesso');
+            res.redirect('/consulta');
         })
-        .catch(error => {
-            // Trata erros ao obter os documentos da coleção
-            console.error('Erro ao obter documentos:', error);
+        .catch((error) => {
+            console.log('Erro ao atualizar o documento: ', error);
+            res.redirect('/consulta');
         });
-});*/
-
-
-
-//---------------------------------------------------------------/
-//criando uma rota para a página de edição
-app.get("/editar/:id", function (req, res) {
-
-})
-
-//---------------------------------------------------------------/
-//criando uma rota para a página de exclusão
-app.get("/deletar/:id", function (req, res) {
-
-})
-
-//---------------------------------------------------------------/
-//criando uma rota para a atualização na consulta
-app.post("/atualizar", function (req, res) {
-
-})
+});
 
 //---------------------------------------------------------------/
 //criando servidor web na porta 8081
 app.listen(8081, function () {
-    console.log("Servidor Ativo!!")
-})
+    console.log("Servidor Ativo!!");
+});
